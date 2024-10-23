f028ebeeef15bf1d17cd72d6e0a9b4db9ee35aaa
innocentemutabazi <innocentemutabazi@gmail.com>
2024-10-24 01:37:02 +0200

    learned data types

--- bebe.java
+++ bebe.java
@@ -1,7 +1,22 @@
 public class bebe {
+ 
   public static void main(String[] args) {
+    int age=19;
+    float networth = 1.6f;
+    final String name = "Innocente Mutabazi Umuhuza";  // The final keyword is like the const keyword in JavaScript it is used to declare variables that can not be reassigned
+    String initial = "I";
+    char letter = 'i';
+    boolean hasBoyfriend = false;
+    String fullIntro;
+    initial="bebe";
     System.out.println("my name is bebe");
     System.out.println("My mom is Joselyne");
-    System.out.println("My brother is called Gitama");
+    System.out.println("My brother is called Gitama"); //the printLn always write the text in a new line 
+    System.out.print("why all of the sudden do I feel good and then bad ");
+    System.out.print("why all of the sudden do I feel good and then bad"); //while the print doesn't start a new line and will always try to fit the print result in one line
+    System.out.print(4 + 0.3);
+    fullIntro = "Hi my name is Innocente Mutabazi Innocente and my networth is 1.6k usd";
+    System.out.println(name);
+    System.out.println(fullIntro);
   }
 }
