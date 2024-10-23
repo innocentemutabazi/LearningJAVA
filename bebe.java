88a144f201c61e0cacdc9e0599d2217f1f0aa3ab
innocentemutabazi <innocentemutabazi@gmail.com>
2024-10-24 01:20:30 +0200

    Learning to print in the console

---
+++ bebe.java
@@ -0,0 +1,7 @@
+public class bebe {
+  public static void main(String[] args) {
+    System.out.println("my name is bebe");
+    System.out.println("My mom is Joselyne");
+    System.out.println("My brother is called Gitama");
+  }
+}
