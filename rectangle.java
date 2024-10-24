320ad61f515d8275a46e1b06973a465c8632dc2e
innocentemutabazi <innocentemutabazi@gmail.com>
2024-10-24 02:06:21 +0200

    calculated per and area of a  rectangle using java

---
+++ rectangle.java
@@ -0,0 +1,12 @@
+public class rectangle {
+  public static void main(String[] args) {
+    int width,length,area,perimeter;
+    width = 10;
+    length= 7;
+    area = width*length;
+    perimeter= 2*(width+length);
+    System.out.println("The area of a triangle whose length is "+ length +" and width of "+ width + " is "+ area);
+    System.out.println("The perimeter of a triangle whose length is "+ length +" and width of "+ width + " is "+ perimeter);
+
+  }
+}
