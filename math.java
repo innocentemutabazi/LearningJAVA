1c4579690eea5f1f5eee15cac1e6524c80d80871
innocentemutabazi <innocentemutabazi@gmail.com>
2024-10-24 09:39:05 +0200

    learned math

---
+++ math.java
@@ -0,0 +1,11 @@
+public class math {
+  public static void main(String[] args) {
+    System.out.println(Math.min(1, 0));
+    System.out.println(Math.max(9, 3));
+    System.out.println(Math.sqrt(9));
+    System.out.println(Math.abs(-5));
+    double rand = Math.random();
+    System.out.println(rand);
+  }
+  
+}
