85f9713ca7216af58fb87b7f83ecde23084c92ff
innocentemutabazi <innocentemutabazi@gmail.com>
2024-10-24 10:03:19 +0200

    learnt if statements

---
+++ booleani.java
@@ -0,0 +1,11 @@
+public class booleani{
+
+  public static void main(String[] args) {
+    int age= 24;
+    int adult=20;
+    if (age >= adult){
+   System.out.println("You are a grown ass woman now");
+    } else System.out.println("enjoy your teenage years");
+
+  }
+}
