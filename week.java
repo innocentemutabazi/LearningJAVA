5042802323f9ea9fce4e0dc6fa098b874381f1e6
innocentemutabazi <innocentemutabazi@gmail.com>
2024-10-24 10:25:03 +0200

    learnt switch case

---
+++ week.java
@@ -0,0 +1,24 @@
+public class week {
+  public static void main(String[] args) {
+      int day = 5;
+      switch (day) {
+          case 1:
+             System.out.println("Monday"); 
+              break;
+              case 2:
+              System.out.println("tuesday"); 
+               break;
+                case 3:
+               System.out.println("wednesday"); 
+                break; 
+                case 4:
+                System.out.println("thursday"); 
+                 break;
+                  case 5:
+                 System.out.println("friday"); 
+                  break;
+                  default: System.out.println("It's a weekend");
+         
+      }
+  }
+}
