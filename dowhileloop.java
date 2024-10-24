3fcd718dab0c5dd428674bca9f3f305c22f158fc
innocentemutabazi <innocentemutabazi@gmail.com>
2024-10-24 13:00:30 +0200

    Tried out the do while loop

---
+++ dowhileloop.java
@@ -0,0 +1,11 @@
+public class dowhileloop {
+  public static void main(String[] args) {
+   int age = 1;
+      do {
+       System.out.println("You're still "+ age + "old");
+       age ++;
+      }
+      while(age < 20);
+
+  }
+}
