734b26c825a55b6f1b58dda45d21be218e22bb75
innocentemutabazi <innocentemutabazi@gmail.com>
2024-10-24 02:14:19 +0200

    learning about numbers

---
+++ numbers.java
@@ -0,0 +1,8 @@
+public class numbers {
+  public static void main(String[] args) {
+    byte num = 100;
+    short num1 = 3000;
+    int num2 = 4000;
+    long num3 = 8899999999999999999999999999999;
+  }
+}
