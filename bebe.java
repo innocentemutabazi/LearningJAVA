4f421eb1c90293a6a101c44d2dd9066a44b39860
innocentemutabazi <innocentemutabazi@gmail.com>
2024-10-24 02:01:55 +0200

    made a student details using java

--- bebe.java
+++ bebe.java
@@ -3,12 +3,13 @@ public class bebe {
   public static void main(String[] args) {
     int age=19;
     float networth = 1.6f;
-    final String name = "Innocente Mutabazi Umuhuza";  // The final keyword is like the const keyword in JavaScript it is used to declare variables that can not be reassigned
-    String initial = "I";
+    // to declare variables of the same datatype we can write the datatype then varible names and values separated by a comma and when we put the final keyword at the beginning it will affect each and every variable that we are assigning in this line it also works on the variable declaration without an assignment
+    final String name = "Innocente Mutabazi Umuhuza", initial = "I",fullIntro; // The final keyword is like the const keyword in JavaScript it is used to declare variables that can not be reassigned
     char letter = 'i';
     boolean hasBoyfriend = false;
-    String fullIntro;
-    initial="bebe";
+    final String papa; //unlike JS's const when we use final in Java we can leave the variable unassigned
+    String x,y,z="bebe";
+    x=y=z;
     System.out.println("my name is bebe");
     System.out.println("My mom is Joselyne");
     System.out.println("My brother is called Gitama"); //the printLn always write the text in a new line 
@@ -18,5 +19,7 @@ public class bebe {
     fullIntro = "Hi my name is " + name + " and my networth is " + networth + " Million Rwf";
     System.out.println(name);
     System.out.println(fullIntro);
+    System.out.println(x);
+    System.out.println("My name is " + name);
   }
 }
---
+++ student.java
@@ -0,0 +1,11 @@
+public class student {
+  public static void main(String[] args) {
+    String name = "Innocente Mutabazi Umuhuza", university="African Leadership University" , course = "Bachelor's in Software Enginnering" , id="C12345";
+    int age = 19;
+    System.out.println("Student's name: " +  name);
+    System.out.println("Student's age: " + age);
+    System.out.println("Student's university: "+ university);
+    System.out.println("Student's major: "+ course);
+    System.out.println("Student's id: "+ id);
+  }
+}
