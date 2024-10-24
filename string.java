10fe121691b70f119adcd594eea48fba62168e56
innocentemutabazi <innocentemutabazi@gmail.com>
2024-10-24 09:21:20 +0200

    learnt more about strings

---
+++ string.java
@@ -0,0 +1,12 @@
+public class string {
+  public static void main(String[] args) {
+    System.out.println("bebe".length());
+    System.out.println("bebe".toUpperCase());
+    System.out.println("Bebe".toLowerCase());
+    System.out.println("My name is Innocente".indexOf("Innocente"));
+    System.out.println("Innocente".concat("Mutabazi"));
+    System.out.println(10+"20");
+    System.out.println("She said \"She loves me\" but i don't know what love means");
+  }
+  
+}
