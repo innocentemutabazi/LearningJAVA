71de05073fd9bf1c4567494835ab7b1c2ee0b45b
innocentemutabazi <innocentemutabazi@gmail.com>
2024-10-24 13:16:28 +0200

    tried the for loop

--- whileloop.java
+++ whileloop.java
@@ -2,9 +2,10 @@ public class whileloop {
   public static void main(String[] args) {
       int age=1;
 
-      while(age == 20){
+      while(age < 20){
         System.err.println("You're just " + age + " old");
         age++;
       }
+      System.out.println("Welcome to womanhood");
   }
 }
