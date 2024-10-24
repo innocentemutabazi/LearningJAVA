e7d2ae34c1d41178e37d5dc6da23f1f3f66f8ae2
innocentemutabazi <innocentemutabazi@gmail.com>
2024-10-24 13:22:44 +0200

    learnt a foreach loop and how it is used and also how arrays look like

---
+++ foreach.java
@@ -0,0 +1,9 @@
+public  class foreach {
+
+  public static void main(String[] args) {
+    String[] family = {"bebe","papa","mama","loic"};
+    for (String i : family){
+      System.out.println(i+ " is a member of Ntidendereza family");
+    }
+  }
+}
